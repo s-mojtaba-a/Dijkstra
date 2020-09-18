e1976703e2d5a5393bc828c40dce60471d084c12
Mojtaba Abedi <68202630+s-mojtaba-a@users.noreply.github.com>
2020-09-18 23:14:53 +0430

    source code

---
+++ Cpp.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void dijkestra(vector<vector<pair<int, int>>> &s,
+               int dis[], int v, int n)
+{
+    // pair<int,int> = {wight , neighbor node}
+    // dijkestra from node v
+    // n = number of nodes
+    fill(dis, dis + n + 1, 1000000000);
+    dis[v] = 0;
+    // min-heap
+    // pair<int,int> = {wight , vertex}
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> Q;
+    Q.push({0, v});
+    // mark boolean array
+    bool mark[n + 1] = {0};
+
+    while (!Q.empty())
+    {
+        pair<int, int> x = Q.top();
+        int w = x.first;
+        int v = x.second;
+        Q.pop();
+        if (mark[v])
+            continue;
+        mark[v] = 1;
+        int len = s[v].size();
+        for (int i = 0; i < len; i++)
+        {
+            if (dis[s[v][i].second] > w + s[v][i].first)
+            {
+                dis[s[v][i].second] = w + s[v][i].first;
+                Q.push({dis[s[v][i].second], s[v][i].second});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    /* int n,v ;
+    cin >> n >> v ;
+    vector<vector<pair<int,int>>> s (n+1) ;
+    int dis[n+1];
+    */
+}
